730bacc3091a6d4c477c1691886790fc2c04dd2c
Mikael <glannstrom@gmail.com>
2026-03-08 16:41:58 +0100

    Add logging for mobile view data state

--- client/src/pages/CoverLetterGenerator.tsx
+++ client/src/pages/CoverLetterGenerator.tsx
@@ -687,6 +687,8 @@ export default function CoverLetterGenerator() {
   // === RENDER ===
   
   // Mobile simplified view
+  console.log('[CoverLetter] Mobile view active. jobbAnnons length:', jobbAnnons.length, 'company:', company, 'jobTitle:', jobTitle)
+  
   if (isMobile) {
     return (
       <div className="max-w-lg mx-auto space-y-4 pb-20 px-4">
